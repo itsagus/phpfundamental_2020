f2535b24a55cc737cdbe34dac67295281fe8c6d6
itsagus <agus.zerone@gmail.com>
2020-06-17 22:53:52 +0800

    PHPFundamental

---
+++ Day6/upload.php
@@ -0,0 +1,21 @@
+<?php 
+//ambil data file
+$namaFile = $_FILES['berkas']['name'];
+$namaSementara = $_FILES['berkas']['tmp_name'];
+
+//tentukan lokasi file akan dipindah
+$dirUpload = "Terupload/";
+
+//pindahkan file
+$terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+
+//
+if ($terupload) {
+	echo "Upload Berhasil! </br>";
+	echo "Link : <a href'".$dirUpload.$namaFile."'>".$namaFile."</a>";
+} else {
+	echo "Upload Gagal!";
+} 
+
+
+ ?>
